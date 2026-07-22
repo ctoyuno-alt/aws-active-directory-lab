e53c7c3d0f3e7ebfea0e7d4dd0c5cfafd04d9a49
ctoyuno-alt <cto.yuno@gmail.com>
2026-07-22 06:41:12 +0000

    Refactor code structure for improved readability and maintainability

--- terraform/modules/monitoring/ssm.tf
+++ terraform/modules/monitoring/ssm.tf
@@ -13,3 +13,26 @@ resource "aws_ssm_association" "install_cloudwatch_agent" {
 
   wait_for_success_timeout_seconds = 600
 }
+
+resource "aws_ssm_association" "configure_cloudwatch_agent" {
+  name = "AmazonCloudWatch-ManageAgent"
+
+  depends_on = [
+    aws_ssm_association.install_cloudwatch_agent
+  ]
+
+  targets {
+    key    = "InstanceIds"
+    values = [var.instance_id]
+  }
+
+  parameters = {
+    action                        = "configure"
+    mode                          = "ec2"
+    optionalConfigurationSource   = "ssm"
+    optionalConfigurationLocation = aws_ssm_parameter.cloudwatch_config.name
+    optionalRestart               = "yes"
+  }
+
+  wait_for_success_timeout_seconds = 600
+}
